9d7325b622daab0186736b81e3b2e9344d6f678a
Russell <russell.smithers@informationcatalyst.com>
2021-09-30 10:07:45 +0100

    Example of code which breaks the liskov substition principle.

---
+++ Liskov/Liskov/LiskovEvilTwin.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Liskov
+{
+    class LiskovEvilTwin
+    {
+        List<GeoShape> shapes = new List<GeoShape>();
+
+        public void AddShape(GeoShape newShape)
+        {
+            shapes.Add(newShape);
+        }
+
+        public void DisplayList()
+        {
+            foreach (GeoShape shape in shapes)
+            {
+                GeoShape.DrawMyShape(shape);
+            }
+        }
+    }
+
+    public enum TypeOfShape { non_shape, square, circle };
+
+    public class GeoShape
+    {
+        private TypeOfShape whichShapeAmI = TypeOfShape.non_shape;
+        
+        internal Tuple<int, int> 
+            position = new Tuple<int , int >(0,0);
+
+        public GeoShape(int x, int y, TypeOfShape myShapeType = TypeOfShape.non_shape) 
+        { 
+            whichShapeAmI = myShapeType;
+            position = new Tuple<int,int>(x, y);
+        }
+
+        public static void DrawMyShape(GeoShape shapeToDraw)
+        {
+            switch (shapeToDraw.whichShapeAmI)
+            {
+                case TypeOfShape.square:
+                    (shapeToDraw as Square).Draw();
+                    break;
+                case TypeOfShape.circle:
+                    (shapeToDraw as Circle).Draw();
+                    break;
+                default: return;
+            }
+        }
+    }
+
+    public class Circle : GeoShape
+    {
+        public Circle(int x, int y) : base(x, y, TypeOfShape.circle) { }
+        public void Draw() { Console.WriteLine($"Circle at {base.position.Item1} , {base.position.Item1}"); }
+    }
+
+    public class Square : GeoShape
+    {
+        public Square(int x, int y) : base(x, y, TypeOfShape.square) { }
+        public void Draw() { Console.WriteLine($"Square at {base.position.Item1} , {base.position.Item1}"); }
+    }
+}
---
+++ Liskov/Liskov/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Liskov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LiskovEvilTwin evilTwin = new LiskovEvilTwin();
+            evilTwin.AddShape(new Circle(10, 10));
+            evilTwin.AddShape(new Square(20, 20));
+            evilTwin.DisplayList();
+        }
+    }
+}
