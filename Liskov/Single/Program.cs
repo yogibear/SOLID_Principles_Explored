95f091f8ef5caabd33279d7b9db44560fd293f94
Russell <russell.smithers@informationcatalyst.com>
2021-10-28 11:20:29 +0100

    Initial changes for SRP violation and other example code

---
+++ Liskov/Single/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Single
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Liskov/Single/SRP_Violation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Single
+{
+    class SRP_Violation
+    {
+
+    }
+}
